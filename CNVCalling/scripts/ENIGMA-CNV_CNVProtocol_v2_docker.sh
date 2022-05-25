709b5fd3644c479187fc57d78e62ec2cee762aca
ida.sonderby <idaeson@idasonderbys-MacBook-Pro.local>
2022-05-25 13:28:22 +0200

    Smaller changes to ENIGMA-CNV_CNVProtocol_v2_docker.sh

--- CNVCalling/scripts/ENIGMA-CNV_CNVProtocol_v2_docker.sh
+++ CNVCalling/scripts/ENIGMA-CNV_CNVProtocol_v2_docker.sh
@@ -54,12 +54,12 @@ START=$(date +%s)
 ##################################################
 
 # General info
-declare Dataset="TOPMD" # Replace TOPMD with the name of your dataset
+declare Dataset="test" # Replace TOPMD with the name of your dataset
 declare ResponsiblePI="OleAndreassen" # Replace OleAndreassen with the name of the PI [no spaces, please]
 declare Email_PI="o.a.andreassen@medisin.uio.no" # Replace o.a.andreassen@medisin.uio.no with the e-mail of your PI to enable us to contact the PI directly in case of questions.
 declare Analyst="IdaSoenderby" # Replace IdaSoenderby with the name of the analyst [no spaces, please]
 declare Email_Analyst="i.e.sonderby@medisin.uio.no" # Replace e-mail with the e-mail of the analyst to enable us to contact the analyst directly in case of questions.
-declare ANALYSISDIR="/cluster/projects/p697/users/idaeson/CNVCalling/enigmacnv/ENIGMA-CNV_Analysis/" # replace with the full path for the Analysis-folder on your computer/server
+declare ANALYSISDIR="/Users/idaeson/home/Projects/ENIGMA_CNV/Protocols/Protocolprep_Winter2021/Testingprotocol_May2022/" # replace with the full path for the Analysis-folder on your computer/server
 declare deidentify="NA" # only change to YES if deidentification is really important to you, otherwise keep NA
 
 # a. Genetic information files - input specific to your genotyping chip
@@ -73,12 +73,12 @@ declare SNPPosFile="339syni_DeCodeGenetics_V1_v2_SNP_Map.txt"
 	# rs1000000	12	126890980
 	# rs1000002	3	183635768
 	# rs10000023	4	95733906
-declare genomeversion="hg19" # Genome-version of SNPpositionfile. It is important that this is correct - otherwise CNV calls and visualization become wrong.
+declare genomeversion="hg18" # Genome-version of SNPpositionfile. It is important that this is correct - otherwise CNV calls and visualization become wrong.
 # IF BAF-LRR-files already present for your dataset (for instance Affy-users)
-declare BAFLRRpresent="no" # USER-INPUT - only put "yes" if you have BAF-LRR-files ready available - otherwise, leave the "no"
+declare BAFLRRpresent="yes" # USER-INPUT - only put "yes" if you have BAF-LRR-files ready available - otherwise, leave the "no"
 if [ $BAFLRRpresent = "yes" ]
     then
-    declare LRRBAFDIR="/cluster/projects/p697/users/idaeson/CNVCalling/TOPMD/IlluminaFinalReport/" # USER-INPUT -  directory with PennCNV-inputfiles
+    declare LRRBAFDIR="/${ANALYSISDIR}/LRRBAF_Files/" # USER-INPUT -  directory with PennCNV-inputfiles
 # NOTE - if the lrr-baf-folder has several subfolders with lrrbaf-files, the script will run into issues... Please contact the helpdesk
     else
     mkdir ${ANALYSISDIR}/LRRBAF_Files/
@@ -93,8 +93,8 @@ declare SexFile="${ANALYSISDIR}/SexFile.txt" # USER-INPUT - absolute path to you
 # If sex for a sample is not provided in sexfile, or if --sexfile is not specified, PennCNV will try to predict the gender of the sample. It is highly recommended to provide a sexfile [saves time].
 declare gendermissing="0" # USER-INPUT, number of individuals with gender missing in sexfile
 declare RelativeFile="${ANALYSISDIR}/DupsRelatives.txt" # USER-INPUT - absolute path to your relative-file
-declare KeepFile="${ANALYSISDIR}/KeepFile.txt" # USER-INPUT # absolute path to your KeepFile - leave empty if you do not have a KeepFile
-declare RemoveFile="${ANALYSISDIR}/RemoveFile.txt" # USER-INPUT # absolute path to your RemoveFile - leave empty if you do not have a RemoveFile
+declare KeepFile="" # USER-INPUT # absolute path to your KeepFile.txt - leave empty if you do not have a KeepFile
+declare RemoveFile="${ANALYSISDIR}/RemoveFile.txt" # USER-INPUT # absolute path to your RemoveFile.txt - leave empty if you do not have a RemoveFile
 
 ### c. Genotyping-chip dependent information - 
 declare HMMname="/opt/PennCNV-1.0.5/lib/hhall.hmm" # Please replace with the correct HMM-file. examples:  /opt/PennCNV-1.0.5/lib/hhall.hmm; for /opt/PennCNV-1.0.5/libhh550.hmm /opt/PennCNV-1.0.5/affy/libgw6/affygw6.hmm
@@ -103,7 +103,7 @@ declare HMMname="/opt/PennCNV-1.0.5/lib/hhall.hmm" # Please replace with the cor
 declare NoofIndividuals="" # e.g "1000" # No of individuals (e.g. "300") to be used for generating PFB-file and GCC-file (must be at least 200 individuals of good quality). Leave empty if you want to use all your individuals. The more individuals you use, the more precise the estimate becomes but the longer it will take to generate the PFB-model. For the NORMENT dataset, generating a PFB- and GCMODEL-file for the OmniExpress12v1.0 containing 730,525 markers using 1000 individuals took ~90 min on a Mac laptop with a 2.53 GHz Intel Core i5 processor and 4 GB of working memory
 
 # IF your dataset contains less than 300 individuals, you need to use a generic version of the PFB-file. Please confer with the ENIGMA-CNV working group and put in the correct names of the files. # NOTE - IF more than 300 individuals, these files will be generated later and named "${Dataset}_${genomeversion}.pfb" (likewise for GCName)
-declare PFB="DeCodeGenetics_V1_20012591_A1_${genomeversion}" # ONLY USER-INPUT for those with <300 individuals -  replace with the correct PFB & GCMODEL (note without extensions) 
+declare PFB="hhall.${genomeversion}" # ONLY USER-INPUT for those with <300 individuals -  replace with the correct PFB & GCMODEL (note without extensions) 
 declare PFBname="${PFB}.pfb"  # no input 
 declare GCname="${PFB}.gcmodel" # no input
 
@@ -114,10 +114,10 @@ declare GCname="${PFB}.gcmodel" # no input
 # a. List of input and output-files from ENIGMA CNV calling protocol
 declare List_preQC="${Dataset}_ListofInputFiles_preQC.txt"
 declare List_postQC="${Dataset}_ListofInputFiles_postQC.txt"
-declare SOFTWAREDIR="${ANALYSISDIR}" # (enigmacnv.sif-placement) OBS - for the majority of cohorts this will be the ${ANALYSISDIR}. Can be replaced with absolut path, e.g. /cluster/projects/p697/users/idaeson/CNVCalling/software/"
+# CAN BE REMOVED?!?!declare SOFTWAREDIR="${ANALYSISDIR}" # (enigmacnv.sif-placement) OBS - for the majority of cohorts this will be the ${ANALYSISDIR}. Can be replaced with absolut path, e.g. /cluster/projects/p697/users/idaeson/CNVCalling/software/"
 
 # b. Cut-offs, autosomal chromosomes filtering
-declare LRR_SD=0.50 
+declare LRR_SD=0.40 
 declare BAF_drift=0.02 
 declare WF=0.05
 declare NoofSNPs=15
@@ -135,7 +135,7 @@ declare WF_X=0.99
 ### d. Parameters needed for visualization
 declare VISUALIZEDIR=${ANALYSISDIR}/${Dataset}_visualize/
 mkdir ${VISUALIZEDIR}
-declare CNVofInterestFile="ENIGMA-CNV_CNVsofInterest_${genomeversion}.csv" # appropriate CNVsofInterest-file for visualization (changes according to genome version)
+declare CNVofInterestFile="CNVsofInterest_ENIGMA-CNV_${genomeversion}.csv" # appropriate CNVsofInterest-file for visualization (changes according to genome version)
 Overlapref=0.3 # How large a proportion of the CNV is overlapping with the CNVofInterest? 
 OverlapMin=0.35 # Minimum overlap
 OverlapMax=5 # Maximum overlap
@@ -182,11 +182,11 @@ OverlapMax=5 # Maximum overlap
 ###############################################################
  
 # These steps convert the Illumina Report files into separate files for each individual with intensity data for each individual sample
-# if [ $BAFLRRpresent != "yes" ]
-# then
-# # a. Split IlluminaReport into one file for each individual sample
-# docker run -v ${ILLUMINAREPORTDIR}:/illuminadir -v ${LRRBAFDIR}:/lrrbafdir  bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5//split_illumina_report.pl --prefix /lrrbafdir/ /illuminadir/${IlluminaReport}
-# fi
+if [ $BAFLRRpresent != "yes" ]
+then
+# a. Split IlluminaReport into one file for each individual sample
+docker run -v ${ILLUMINAREPORTDIR}:/illuminadir -v ${LRRBAFDIR}:/lrrbafdir  bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5//split_illumina_report.pl --prefix /lrrbafdir/ /illuminadir/${IlluminaReport}
+fi
 	#  "-prefix"	used to specify the prefix of the file name (in this case, save the file to the ${ANALYSISDIR}/LRRBAF_Files/-directory). 
 	#  "-comma"	can be added to the command in case the genotyping center provide the report file in comma-delimited format rather than tab-delimited format
  
@@ -234,20 +234,22 @@ sed -i -e 's/.*\//\/lrrbafdir\//' ${ANALYSISDIR}/${List_postQC}_adj
 	# The script compile_pfb_new.pl compiles a PFB file from multiple signal intensity files containing BAF values	 
 	# The script cal_gc_snp.pl calculates GC content surrounding each marker within specified sliding window, using the UCSC GC annotation file.
 
-declare length_fileinput=`wc -l ${List_postQC} | awk '{print $1}'` # number of individuals in 
+declare length_fileinput=`wc -l ${List_postQC} | awk '{print $1}'` # number of individuals in
 
 if [ ${length_fileinput} >299 ]
+  	echo "more than 299 individuals, a PFB-file for your dataset will be created"
+  fi
 	then
 	declare PFBname="${Dataset}_${genomeversion}.pfb" # PFB-file
 	declare GCname="${Dataset}_${genomeversion}.gcmodel" # GC-model-file
 	declare List_Helperfiles="${Dataset}_ListofInputFiles_PFB.txt" # files used for input for PFB
-	
+
 	if [ -n "${NoofIndividuals}" ] ## variable exists and is not empty
 	then
-		echo "number of individuals used for generating PFB-file: ${NoofIndividuals}"	
+		echo "number of individuals used for generating PFB-file: ${NoofIndividuals}"
 	else
 		declare NoofIndividuals=${length_fileinput}
-		echo "number of individuals used for generating PFB-file: ${NoofIndividuals}"	
+		echo "number of individuals used for generating PFB-file: ${NoofIndividuals}"
 	fi
 		# a. Generate a list of x individuals randomly selected from your file
 		export NoofIndividuals; awk 'BEGIN {p=ENVIRON["NoofIndividuals"]; srand();} {a[NR]=$0} END{for(i=1; i<=p; i++){x=int(rand()*NR) + 1; print a[x]}}' ${ANALYSISDIR}/${List_postQC} >${ANALYSISDIR}/${List_Helperfiles}
@@ -263,8 +265,8 @@ if [ ${length_fileinput} >299 ]
 		gunzip ${ANALYSISDIR}/${genomeversion}.gc5Base.txt.gz
 		docker run -v ${ANALYSISDIR}:/analysisdir bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5/cal_gc_snp.pl /analysisdir/${genomeversion}.gc5Base.txt /analysisdir/${PFBname} -output /analysisdir/${GCname}
 		rm ${ANALYSISDIR}/${genomeversion}.gc5Base.txt
-else	
-	echo "less than 200 individuals, please choose a generic PFB-file after conferring with the ENIGMA-CNV working group"
+else
+	echo "less than 300 individuals, please choose a generic PFB-file after conferring with the ENIGMA-CNV working group"
 fi
 
 #######################
@@ -274,35 +276,36 @@ fi
 # Autosomal and X-chromosome CNVs are called separately
 
 ## Step 1: Call CNVs on autosomal chromosomes with GC-adjustment
-docker run -v ${ANALYSISDIR}:/analysisdir -v ${LRRBAFDIR}:/lrrbafdir bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5/detect_cnv.pl -test -hmm ${HMMname} -pfb /analysisdir/${PFBname} -gcmodel /analysisdir/${GCname} -list /analysisdir/${List_postQC}_adj --confidence -out /analysisdir/${Dataset}.auto.raw  -log /analysisdir/${Dataset}.auto.raw.log 
-	# --test		tells the program to generate CNV calls 
+docker run -v ${ANALYSISDIR}:/analysisdir -v ${LRRBAFDIR}:/lrrbafdir bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5/detect_cnv.pl -test -hmm ${HMMname} -pfb /analysisdir/${PFBname} -gcmodel /analysisdir/${GCname} -list /analysisdir/${List_postQC}_adj --confidence -out /analysisdir/${Dataset}.auto.raw  -log /analysisdir/${Dataset}.auto.raw.log
+
+	# --test		tells the program to generate CNV calls
 	# --confidence	calculate confidence for each CNV
 	# --hmmfile and –pfbfile	provides the files for hmm- & pfb-files
 	# --gcmodel	implements a wave adjustment procedure for genomic waves which reduce false positive calls. It is recommended when if a large fraction of your samples have waviness factor (WF value) less than -0.04 or higher than 0.04
 	# -list		provides the file with the list of samples you want called
-	# –log		tells the program to write log information 
+	# –log		tells the program to write log information
 	# --out		raw cnvcalls for all input individuals
 
-# Note: This took ~48 hours to run for the MAS sample which comprises 925 individuals and 1,8 mio marker on the Affymetrix genomewide array 6.0-platform. 
+# Note: This took ~48 hours to run for the MAS sample which comprises 925 individuals and 1,8 mio marker on the Affymetrix genomewide array 6.0-platform.
 
- 
+
 ## Step 2: Call CNVs on X-chromosome with GC-adjustment
 
 # a. Make sex-file for your inputfile
-awk '{print $1,$1}' ${List_postQC}_adj | awk -v postscript=${postscript} -v prescript=${prescript} '{sub(/.+\//,"\/",$1); sub(/\//,"",$1); gsub(postscript,"",$1); gsub(prescript,"",$1); print}' | awk 'FNR==NR {k[$1]=$2;next} {if ($1 in k) {print k[$1] "\t" $2}}' - ${SexFile} >${ANALYSISDIR}/${Dataset}_SexFile.txt 
+awk '{print $1,$1}' ${List_postQC}_adj | awk -v postscript=${postscript} -v prescript=${prescript} '{sub(/.+\//,"\/",$1); sub(/\//,"",$1); gsub(postscript,"",$1); gsub(prescript,"",$1); print}' | awk 'FNR==NR {k[$1]=$2;next} {if ($1 in k) {print k[$1] "\t" $2}}' - ${SexFile} >${ANALYSISDIR}/${Dataset}_SexFile.txt
 
     # If sex for a sample is not provided in sexfile, or if --sexfile is not specified, PennCNV will try to predict the gender of the sample. It is highly recommended to provide a sexfile [saves time].
     # This command couples the full path of each individual input-file to sex with the following end-format:
     # 	/Volumes/CNV_Calling/Analysis/PennCNV_InputFiles/TOP1	female
     # 	/Volumes/CNV_Calling/Analysis/PennCNV_InputFiles/TOP2	male
     # 	etc
- 
+
 # b. Call CNVs on X-chromosome
 docker run -v ${ANALYSISDIR}:/analysisdir -v ${LRRBAFDIR}:/lrrbafdir  bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5/detect_cnv.pl -test -hmm ${HMMname} -pfb /analysisdir/${PFBname} -gcmodel /analysisdir/${GCname} -list /analysisdir/${List_postQC}_adj --confidence -out /analysisdir/${Dataset}.X.raw  -log /analysisdir/${Dataset}.X.raw.log --chrx --sexfile /analysisdir/${Dataset}_SexFile.txt
 	# --chrx		specifies that x-chromosome should be called
 	# --sexfile	provides the sexfile data for the calling
 
-# Note: This took ~48 hours to run for the MAS sample which comprises 925 individuals and 1,8 mio marker on the Affymetrix genomewide array 6.0-platform on a ??? specify computer etc... 
+# Note: This took ~48 hours to run for the MAS sample which comprises 925 individuals and 1,8 mio marker on the Affymetrix genomewide array 6.0-platform on a ??? specify computer etc...
 
 ######################
 ## D. Downstream QC ##
@@ -313,7 +316,7 @@ docker run -v ${ANALYSISDIR}:/analysisdir -v ${LRRBAFDIR}:/lrrbafdir  bayramalex
 ###########
 
 # The raw CNV calls need to have calls from low quality samples eliminated and low quality calls need to be removed.
- 
+
 # AUTOSOMAL CNVs:
 # Step 1: QC on CNVs, 1st round. Obtain summary statistics
 # Step 2. Merge CNVs
@@ -375,11 +378,11 @@ done
 echo "Finished merging of autosomal CNVs"
 
 # remove the resulting merging files
-if 
+if
 	test -f "${ANALYSISDIR}/${Dataset}.auto.flr_mrg[0-9]"; then
 	rm ${ANALYSISDIR}/${Dataset}.auto.flr_mrg[0-9]
 fi
-if 
+if
 	test -f "${ANALYSISDIR}/${Dataset}.auto.flr_mrg[0-9][0-9]"; then
 	rm ${ANALYSISDIR}/${Dataset}.auto.flr_mrg[0-9][0-9]
 fi
@@ -393,8 +396,8 @@ fi
 
 # i. Identify CNVs with overlap to centromeric, telomeric, segmentalduplication and immunoglobulin regions
 for i in centro telo segmentaldups immuno;
-do 
-	docker run -v  ${ANALYSISDIR}:/analysisdir bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5/scan_region.pl /analysisdir/${Dataset}.auto.flr_mrg_final /analysisdir/${i}_hg38.txt -minqueryfrac ${MinQueryFrac} >${ANALYSISDIR}/${Dataset}.auto.${i};
+do
+	docker run -v  ${ANALYSISDIR}:/analysisdir bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5/scan_region.pl /analysisdir/${Dataset}.auto.flr_mrg_final /analysisdir/${i}_${genomeversion}.txt -minqueryfrac ${MinQueryFrac} >${ANALYSISDIR}/${Dataset}.auto.${i};
 echo "${i} is done";
 done
 
@@ -402,7 +405,7 @@ done
 
 # i. Make list
 for i in centro telo segmentaldups immuno;
-do 
+do
 	cat ${ANALYSISDIR}/${Dataset}.auto.${i};
 done >${ANALYSISDIR}/${Dataset}_SpuriousCNVs_exclude
 
@@ -426,7 +429,7 @@ docker run -v  ${ANALYSISDIR}:/analysisdir bayramalex/enigma-cnv:latest /opt/Pen
 ###########################################################
 
 # a. obtain summary statistics for dataset
-docker run -v ${ANALYSISDIR}:/analysisdir   bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5/filter_cnv.pl /analysisdir/${Dataset}.X.raw --qclrrsd ${LRR_SD_X} --qcbafdrift ${BAF_drift_X} --qcwf ${WF_X} --numsnp ${NoofSNPs_X} --qclogfile /analysisdir/${Dataset}.X.raw.log --qcpassout /analysisdir/${Dataset}.X.passout --qcsumout /analysisdir/${Dataset}.X.sumout --out /analysisdir/${Dataset}.X.flr --chrx 
+docker run -v ${ANALYSISDIR}:/analysisdir   bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5/filter_cnv.pl /analysisdir/${Dataset}.X.raw --qclrrsd ${LRR_SD_X} --qcbafdrift ${BAF_drift_X} --qcwf ${WF_X} --numsnp ${NoofSNPs_X} --qclogfile /analysisdir/${Dataset}.X.raw.log --qcpassout /analysisdir/${Dataset}.X.passout --qcsumout /analysisdir/${Dataset}.X.sumout --out /analysisdir/${Dataset}.X.flr --chrx
 
 echo "Finished first filtering of X-chromosomal CNVs"
 
@@ -475,7 +478,7 @@ fi
 # i. Identify CNVs with overlap to centromeric, telomeric and  segmentalduplication regions
 for i in centro telo segmentaldups immuno;
 do
-docker run -v  ${ANALYSISDIR}:/analysisdir bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5/scan_region.pl /analysisdir/${Dataset}.X.flr_mrg_final /analysisdir/${i}_hg38.txt -minqueryfrac ${MinQueryFrac} >${ANALYSISDIR}/${Dataset}.X.${i};
+docker run -v  ${ANALYSISDIR}:/analysisdir bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5/scan_region.pl /analysisdir/${Dataset}.X.flr_mrg_final /analysisdir/${i}_${genomeversion}.txt -minqueryfrac ${MinQueryFrac} >${ANALYSISDIR}/${Dataset}.X.${i};
 echo "${i} is done";
 done
 
@@ -499,7 +502,7 @@ echo "Finished removal of spurious regions for autosomal CNVs"
 awk 'FNR==NR {a[$1]; next} {if ($5 in a) {print}}' ${ANALYSISDIR}/${Dataset}.auto.passout_QC ${ANALYSISDIR}/${Dataset}.X.flr_mrg_spur >${ANALYSISDIR}/${Dataset}.X.flr_mrg_spur_onlypass
 
 # b. obtain summary statistics for dataset
-docker run -v  ${ANALYSISDIR}:/analysisdir bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5/filter_cnv.pl /analysisdir/${Dataset}.X.flr_mrg_spur_onlypass -qclrrsd ${LRR_SD_X} --qcbafdrift ${BAF_drift_X} --qcwf ${WF_X} -numsnp ${NoofSNPs_X} -qclogfile /analysisdir/${Dataset}.X.raw.log -qcpassout /analysisdir/${Dataset}.X.passout_QC -qcsumout /analysisdir/${Dataset}.X.sumout_QC -out /analysisdir/${Dataset}.X.flr_QC --chrx 
+docker run -v  ${ANALYSISDIR}:/analysisdir bayramalex/enigma-cnv:latest /opt/PennCNV-1.0.5/filter_cnv.pl /analysisdir/${Dataset}.X.flr_mrg_spur_onlypass -qclrrsd ${LRR_SD_X} --qcbafdrift ${BAF_drift_X} --qcwf ${WF_X} -numsnp ${NoofSNPs_X} -qclogfile /analysisdir/${Dataset}.X.raw.log -qcpassout /analysisdir/${Dataset}.X.passout_QC -qcsumout /analysisdir/${Dataset}.X.sumout_QC -out /analysisdir/${Dataset}.X.flr_QC --chrx
 
 ##########################
 ## BOTH AUTOSOMAL AND X ##
